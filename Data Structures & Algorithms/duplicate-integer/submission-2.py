8f8834f4f9b6566deb601089a99946f049ecad11
Krishna Bashyal <85485447+Krishnabasyal23@users.noreply.github.com>
2026-05-13 13:27:53 -0400

    question added
    Added a docstring to describe the function's purpose.

--- Data Structures & Algorithms/duplicate-integer/submission-2.py
+++ Data Structures & Algorithms/duplicate-integer/submission-2.py
@@ -1,3 +1,5 @@
+#Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
         hashset=set()
